1ae55662e1b857fe680467891b2e3e6ef468c4ee
nakul1010 <nakul.amate@gmail.com>
2022-04-07 21:00:25 +0530

    fmt in multiply

--- subtract/src/lib.rs
+++ subtract/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn main() {
     println!("In Subtract");
 }
+
+#[cfg(test)]
+mod test{
+
+    #[test]
+    fn right_test()
+    {
+        assert_eq!(2,2);
+    }
+
+    fn wrong_test()
+    {
+        assert_eq!(2,4);
+    }
+}
